c2c8b5d3086f2f46f61c25421dbeff45bf686a00
Anderson Dário <andersonwebwa@hotmail.com>
2017-05-24 15:41:10 -0300

    first commit

---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class ArticlesControllerTest < ActionController::TestCase
+  setup do
+    @article = articles(:one)
+    @article_2 = articles(:two)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:articles)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference('Article.count') do
+      post :create, article: { content: @article.content, title: @article.title }
+    end
+
+    assert_redirected_to articles_path
+  end
+
+  test "should show article" do
+    get :show, id: @article
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @article
+    assert_response :success
+  end
+
+  test "should update article" do
+    patch :update, id: @article, article: { content: @article.content, title: @article.title }
+    assert_redirected_to article_path(assigns(:article))
+  end
+
+  test "should destroy article" do
+    assert_difference('Article.count', -1) do
+      delete :destroy, id: @article
+    end
+
+    assert_redirected_to articles_path
+  end
+
+  test "should not create article with too short title" do
+    assert_no_difference('Article.count') do
+      post :create, article: { content: @article_2.content, title: @article_2.title }
+    end
+
+    assert_template :new
+  end
+end
